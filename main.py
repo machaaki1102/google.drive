49857618b31a06ae72f94b314bfc03459bcbeb4b
github <github>
2022-08-25 21:38:08 +0900

    ddd

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 import streamlit as st
+#画像を簡単に処理するライブラリ
+from PIL import Image, ImageFilter
 
 gauth = GoogleAuth()
 gauth.LocalWebserverAuth()
@@ -9,7 +11,8 @@ drive = GoogleDrive(gauth)
 
 name = st.text_input('name')
 field = st.file_uploader('field')
-st.text(type(field))
+im = Image.open(field)
+st.text(type(im))
 close = st.file_uploader('close')
 st.text(type(close))
 
