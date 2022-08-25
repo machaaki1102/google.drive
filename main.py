5b31410185bfe2413c5c7ec5a91a99b597483587
github <github>
2022-08-25 20:46:08 +0900

    ddd

---
+++ main.py
@@ -0,0 +1,11 @@
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+gauth = GoogleAuth()
+gauth.LocalWebserverAuth()
+
+drive = GoogleDrive(gauth)
+
+f = drive.CreateFile({'title': 'TEST.TXT'})
+f.SetContentString('Hello')
+f.Upload()
