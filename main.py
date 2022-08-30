da9df09bf0da083783bd3d8c539935312d719bc6
github <github>
2022-08-30 19:57:32 +0900

    ｓｓ

--- main.py
+++ main.py
@@ -35,8 +35,7 @@ if field:
     #st.text(img_path)
     st.text(os.getcwd())
     st.text(field.name)
-    f = dirve.CreateFile({'title': field.name,
-                         'mimetype':'image/png'})
+    f = dirve.CreateFile({'title':field.name,'mimeType':'image/png'})
     f.SetContentFile(field.name)
     f.Upload()
     #with open('field.png','wb') as f:
