2ef6eafdc1eebce1b10b1523628f6483e1cdb7cf
github <github>
2022-08-31 23:14:45 +0900

    ss

--- main.py
+++ main.py
@@ -36,8 +36,8 @@ for f in file_list:
 if  button3:
     file_id = drive.ListFile({'q': 'title = "image.jpg"'}).GetList()[0]['id']
     f = drive.CreateFile({'id': file_id})
-    f.GetContentFile('/app/google.drive/folers/image.jpg')
-    
+    f.GetContentFile('/app/google/image.jpg')
+
 #  request = drive.files().get_media(fileId='1MqCvA3bM9HWueE39j4Y0MmRsrYXFXjNO')
 #    fh = io.FileIO(file['image.jpeg'],mode='wb')
 #    downloader = MediaIoBaseDownload(fh,request)
