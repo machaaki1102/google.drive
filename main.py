fcb4460ffb13b3f46f32c993c04924a6d387e69c
github <github>
2022-09-02 18:21:15 +0900

    ss

--- main.py
+++ main.py
@@ -94,7 +94,7 @@ if csv:
     clomus = ['id','title','ki','number','long','spad','picture1','picture2']
     #data = np.array([id_id,title_t,ki,name_id,data_long,data_spat,2,1])
     data = [1,2,3,4,5,7,2,1]
-    df1 = pd.DateFrame(data = data)
+    df1 = pd.DataFrame(data = data)
     st.DateFrame(df1)
 
 
