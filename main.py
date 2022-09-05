bbc7d85d73ed2081e0ebc2e520fbbd3c80ce8ec8
github <github>
2022-09-05 21:27:43 +0900

    ss

--- main.py
+++ main.py
@@ -92,7 +92,7 @@ if button_upload:
 csv = st.button('csv')
 if csv:
     colmuns = ['id','title','ki','number','long','spad','picture1','picture2']
-    data = [[id_id,title_t,ki,name_id,data_long,data_spat,2,1]]
+    data = [[id_id,title_t,ki,name_id,data_long,data_spat,fx,fb]]
     #data = [1,2,3,4,5,7,2,1]
     df1 = pd.DataFrame(data = data,columns=colmuns)
     st.dataframe(df1)
