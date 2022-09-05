41122859f7534bdcdcae2229a188deed099f4b8f
github <github>
2022-09-05 22:14:41 +0900

    dd

--- main.py
+++ main.py
@@ -93,6 +93,7 @@ if button_upload:
     colmuns = ['id','title','ki','number','long','spad','picture1','picture2']
     data = [[id_id,title_t,ki,name_id,data_long,data_spat,fx,fb]]
     df1 = pd.DataFrame(data = data,columns=colmuns)
+    st.dataframe(df1)
     
     with open('df_csv','wb') as f:
         f.write(df1.read())
