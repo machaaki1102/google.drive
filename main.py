9102fabf9a62b56b4765677afb146fda03cb4f49
github <github>
2022-09-06 22:00:58 +0900

    ss

--- main.py
+++ main.py
@@ -65,6 +65,7 @@ if  button_download:
     f = drive.CreateFile({'id': file_id})
     #ファイルを読み込み 絶対パスで
     f.GetContentFile(f['title'])
+    st.text(type(f))
     reader = csv.reader(f)
     st.write(reader)
     f.clear()
