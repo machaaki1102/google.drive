782e6a73c79320d944ee67acce2c8431a785ffcd
github <github>
2022-09-06 19:43:30 +0900

    ss

--- main.py
+++ main.py
@@ -94,8 +94,8 @@ if button_upload:
     colmuns = ['id','title','ki','number','long','spad','picture1','picture2']
     data = [[id_id,title_t,ki,name_id,data_long,data_spat,fx,fb]]
     df1 = pd.DataFrame(data = data,columns=colmuns)
-    df_1 = df1.to_csv('df_csv')
-    st.write(df_1)
+    #df_1 = df1.to_csv('df_csv')
+    st.write(df1.to_csv('df_csv'))
     #データフレームからCSVに変えて、ファイルをREAD出来るのか？検討中
     
     with open('df_csv','w') as f:
