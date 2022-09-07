ca5a5cd4a079e6cba8fb0e0ecb62fd2d119ec163
github <github>
2022-09-07 20:54:00 +0900

    dd

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ if  button_download:
     #GOOGLEドライブデータをCSVに変換
     with open('df.csv','r') as f:
         df = pd.DataFrame(f)
-        df_i = string.split(df.iloc[0,:])
+        df_i = df.split(',')
         st.text(df_i)
 #    st.dataframe('df.csv')#byteデータのみ
 #    f.clear()
