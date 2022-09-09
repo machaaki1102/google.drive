01a3afc0f4226912258409336009d3e64e08db24
github <github>
2022-09-10 00:28:01 +0900

    dd

--- main.py
+++ main.py
@@ -255,7 +255,7 @@ if genre == '編集':
     file_id = drive.ListFile({'q': 'title contains "field"'}).GetList()[0]['id']
     f  = drive.CreateFile({'id': file_id})#ファイルを読み込みして、見えないカレントディレクトリ内に見えないが保存されている。絶対パスで
     f.GetContentFile(download_name_a)
-    st.image(download_name_a)
+    #st.image(download_name_a)
     file_id = drive.ListFile({'q': 'title contains "close"'}).GetList()[0]['id']
     f  = drive.CreateFile({'id': file_id})#ファイルを読み込みして、見えないカレントディレクトリ内に見えないが保存されている。絶対パスで
     f.GetContentFile(download_name_b)
