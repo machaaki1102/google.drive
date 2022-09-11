10dea82f95fc5e9ccbc34b40dca515bf94bed98b
github <github>
2022-09-12 06:29:25 +0900

    ss

--- main.py
+++ main.py
@@ -293,8 +293,13 @@ if genre == '編集':
         st.dataframe(df)
         #streamlit 内にあるdf.csvを変更するデータフレームで変更した点をCSVに変換して上書きする
         df.to_csv('df.csv',index=False)
-        with open('df.csv','r')as f:
-            st.dataframe(f)
+        file_id = drive.ListFile({'q': 'title contains "df.csv"'}).GetList()[0]['id']
+        f  = drive.CreateFile({'id': file_id})#ファイルを読み込みして、見えないカレントディレクトリ内に見えないが保存されている。絶対パスで
+        f.SetContentFile(df.csv)
+        f.Upload()
+        f.close
+        #with open('df.csv','r')as f:
+        #    st.dataframe(f)
     #   with open(df.csv,'r') as f
 
         
