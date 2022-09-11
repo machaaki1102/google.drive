ba820c4d2ab4da4af5ceae3bd470cf7eead5109e
github <github>
2022-09-12 06:08:31 +0900

    dd

--- main.py
+++ main.py
@@ -290,7 +290,7 @@ if genre == '編集':
         
 
         df.iloc[df[df['id'] == select].index.to_numpy()[0]] = data_henkou
-        st.text(df)
+        st.dataframe(df)
         #streamlit 内にあるdf.csvを変更するデータフレームで変更した点をCSVに変換して上書きする
         #df.to_csv()
     #    with open(df.csv,'r') as f
