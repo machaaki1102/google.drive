57e4481f755772331ab1748b1ac9df95d5665d26
github <github>
2022-09-13 21:52:09 +0900

    dd

--- main.py
+++ main.py
@@ -185,13 +185,16 @@ if genre == '新規入力':
 #編集画面
 if genre == '編集':
     st.dataframe(df)
+    select = st.selectbox('id', df['id'])
+    st.text(select)#選択したIDから行のINDEXを取得
+    st.text(df['title'][df['id'] == select][1])
+    st.text(df['title'][df['id'] == select][2])
     col1, col2 = st.columns(2)
     with col1:
         select = st.selectbox('id', df['id'])
-        st.text(select)#選択したIDから行のINDEXを取得
-        st.text(df['title'][df['id'] == select][1])
-        st.text(df['title'][df['id'] == select][2])
-        #
+        #st.text(select)#選択したIDから行のINDEXを取得
+        #st.text(df['title'][df['id'] == select][])
+        #st.text(df['title'][df['id'] == select][2])
         #st.text(df[df['id'] == select].index.to_numpy()[0])
         #main_id2 = df['main_id'][df['id'] == select][0]
         title_2 = df['title'][df['id'] == select][0]
